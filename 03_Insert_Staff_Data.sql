dad491acea3d239bf46c3397d8e677afd172fedb
Wisdom-esc <wisdomnwabuche@gmail.com>
2026-07-24 22:16:12 +0100

    Insert staff data into the database

---
+++ 03_Insert_Staff_Data.sql
@@ -0,0 +1,10 @@
+USE wisdomscompany;
+
+INSERT INTO staff(staffId,firstName,lastName,gender,salary,DateOfEmpl)
+VALUES
+("Ab-123","John","Doe","Male",400000,"2002-10-22"),
+("Ab-124","Faith","Hakim","Female",50000,"2004-10-22"),
+("Ab-125","Holland","Agozie","Male",100000,"2005-6-21"),
+("Ab-127","Onyema","Silas","Male",7000000,"2005-9-13"),
+("Ab-128","Chika","Ferdinard","Male",320000,"2006-8-29"),
+("AB-129","Celina","Raaj","Female",90000,"2006-8-30");
